8246c4725de4e4b03ffa44fab4715f22912e1606
freshchen <charles_ling1984@163.com>
2020-02-19 19:15:28 +0800

    [update]

--- java/java8/src/test/java/util/StringsTest.java
+++ java/java8/src/test/java/util/StringsTest.java
@@ -10,6 +10,6 @@ public class StringsTest {
     @Test
     public void capitalizeFirstLetter() {
         String alone = Strings.capitalizeFirstLetter("alone");
-        Assert.assertEquals("Alone" ,alone);
+        Assert.assertEquals("Alone", alone);
     }
 }
