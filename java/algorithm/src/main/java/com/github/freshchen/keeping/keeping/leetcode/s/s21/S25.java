43afde94574a329f7e50e50762acc3845d28004c
freshchen <charles_ling1984@163.com>
2021-03-22 16:19:53 +0800

    [fresh] update

---
+++ java/algorithm/src/main/java/com/github/freshchen/keeping/keeping/leetcode/s/s21/S25.java
@@ -0,0 +1,60 @@
+package com.github.freshchen.keeping.keeping.leetcode.s.s21;
+
+import com.github.freshchen.keeping.keeping.annotation.Link;
+import com.github.freshchen.keeping.keeping.common.ListNode;
+
+import java.util.Objects;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author darcy
+ * @since 2021/3/15
+ **/
+@Link("合并两个有序链表")
+public class S25 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (Objects.isNull(l1)) {
+            return l2;
+        }
+        if (Objects.isNull(l2)) {
+            return l1;
+        }
+        if (l1.val > l2.val) {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        } else {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+    }
+}
