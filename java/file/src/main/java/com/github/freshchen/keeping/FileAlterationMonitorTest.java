025b6ca2cf55a8808b9f34f5deb384c9e206e78a
freshchen <charles_ling1984@163.com>
2021-04-30 15:24:35 +0800

    [fresh] update

---
+++ java/file/src/main/java/com/github/freshchen/keeping/FileAlterationMonitorTest.java
@@ -0,0 +1,29 @@
+package com.github.freshchen.keeping;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+/**
+ * @author darcy
+ * @since 2021/4/30
+ **/
+public class FileAlterationMonitorTest {
+
+    public static void main(String[] args) {
+        FileAlterationMonitor monitor = new FileAlterationMonitor(10);
+        // 文件观察者
+        FileAlterationObserver observer = new FileAlterationObserver(
+                "/Users/chenling/Code/fresh-keeping/java/file/src/main/java/com/github/freshchen/keeping",
+                FileFilterUtils.suffixFileFilter(".txt"));
+        // 监听到文件变动后的处理器
+        observer.addListener(new MyFileListenerAdaptor());
+        observer.addListener(new My1FileListenerAdaptor());
+        monitor.addObserver(observer);
+        try {
+            monitor.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/file/src/main/java/com/github/freshchen/keeping/My1FileListenerAdaptor.java
@@ -0,0 +1,30 @@
+package com.github.freshchen.keeping;
+
+import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
+
+import java.io.File;
+
+/**
+ * @author darcy
+ * @since 2021/4/30
+ **/
+public class My1FileListenerAdaptor extends FileAlterationListenerAdaptor {
+
+    @Override
+    public void onFileChange(File file) {
+        System.out.println("文件 " + file.getName() + "发生变化发生变化");
+        super.onFileChange(file);
+    }
+
+    @Override
+    public void onFileDelete(File file) {
+        System.out.println("文件 " + file.getName() + "被删除了被删除了");
+        super.onFileDelete(file);
+    }
+
+    @Override
+    public void onFileCreate(File file) {
+        System.out.println("文件 " + file.getName() + "被新建了被新建了");
+        super.onFileCreate(file);
+    }
+}
