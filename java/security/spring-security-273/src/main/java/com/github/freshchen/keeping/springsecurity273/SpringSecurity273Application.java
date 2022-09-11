b336d5c533b291eee1d55fd850d079b14b4fc15e
freshchen <961011595@qq.com>
2022-09-11 10:31:38 +0800

    move

---
+++ java/security/spring-security-273/src/main/java/com/github/freshchen/keeping/springsecurity273/SpringSecurity273Application.java
@@ -0,0 +1,13 @@
+package com.github.freshchen.keeping.springsecurity273;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity273Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity273Application.class, args);
+	}
+
+}
---
+++ java/security/spring-security-273/src/test/java/com/github/freshchen/keeping/springsecurity273/SpringSecurity273ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.freshchen.keeping.springsecurity273;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity273ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
